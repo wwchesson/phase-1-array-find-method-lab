032825bf44f4afa13f9ca2772956928e29b6245d
Will <willchesson@gmail.com>
2022-01-20 17:04:53 -0500

    commit tree f6cc4ca0cfefa4e8b1fbe43cc0cc0b2423d820f8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(record) {
+
+    let item = record.find(object => object.result === "W")
+    if (item) {
+        return item.year
+    }
+}
+
+
+
+
+
+
+
+
